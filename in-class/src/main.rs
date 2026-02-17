8e353077fa2179abdc8ce1248de4cba2e244aed4
MDH-beep <michael.dehoyos01@utrgv.edu>
2026-02-17 05:23:25 +0000

    Assignment 1 finish

--- in-class/src/main.rs
+++ in-class/src/main.rs
@@ -10,7 +10,27 @@ fn celsius_to_fahrenheit(c:f64)-> f64 {
     
 }
 
+fn is_even (n: i32) -> bool
+{ if n % 2 == 0  
+{return true;}
+else {
+return false;}
+}
+
+fn check_guess(guess: i32, secret: i32) -> i32
+{
+    if guess == secret {
+        return 0;}
+        else if guess > secret {
+            return 1;}
+        else {
+            return -1;
+        }
+        }  
+
+
 fn main() {
+    // Assignment 1
    const W: u32= 32;
    let mut x: f64 = 50.0;
    let mut counter = 6;
@@ -27,10 +47,88 @@ fn main() {
     y += 1.0;
     counter -= 1;
    }
-
-   fn is_even(n: i32) -> bool
-   {
-    
-   }
-
+// Assignment 2
+  let numbers = [ 2, 22, 54, 66, 20, 15, 40, 12, 6, 1];
+  for num in numbers.iter()
+  // for loop
+  {
+  if num % 3 == 0 && num % 5 == 0 {
+        println!("FizzBuzz");
+    } 
+    else if num % 3 == 0 {
+        println!("Fizz");
+    } 
+    else if num % 5 == 0 {
+        println!("Buzz");
+    } 
+    else {
+        if is_even(*num)
+        {
+            println!("Even :{}",num);
+        } 
+        else 
+        {
+            println!("Odd :{}",num);
+        }
+    }
 }
+ // while loop
+    let mut counter = 0;
+    let mut sum = 0;
+    while counter < numbers.len() 
+    {
+        sum += numbers[counter];
+        counter += 1;
+    }
+    println!("Total sum : {}", sum);
+
+    // largest number loop
+     let mut index = 0;
+    let mut largest = numbers[0];
+
+    loop {
+        if index >= numbers.len() {
+            break;
+        }
+
+        if numbers[index] > largest {
+            largest = numbers[index];
+        }
+
+        index += 1;
+    }
+
+    println!("Largest number: {}", largest);
+
+    // Assignment 3
+     let secret = 22;
+      let mut counter =0;
+      let mut guess;
+
+      loop {
+        counter += 1;
+
+        if counter == 1 {
+            guess = 11;
+        }
+        else if counter == 2 {
+            guess = 25;
+        }
+        else {
+            guess = 22;
+        }
+         let result = check_guess(guess, secret);
+        println!("Guess: {}, Result: {}", guess, result);
+        if result == 0 {
+        println!("Amount of guesses: {}", counter);
+            break;
+        }
+        
+      }
+}
+ 
+
+
+   
+
+
